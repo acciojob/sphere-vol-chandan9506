8d311f4044fccee3231c05c1f88ee7cb6edec2d3
chandan9506 <152258839+chandan9506@users.noreply.github.com>
2024-04-22 09:35:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -8,8 +8,8 @@
 
         const radius = parseFloat(document.getElementById('radius').value);
         if (isNaN(radius) || radius <= 0) {
-            alert('Please enter a valid positive number for the radius.');
-                return;
+             document.getElementById('volume').value = NaN;
+               
             }
         const volume = ((4 / 3) * Math.PI * Math.pow(radius, 3)).toFixed(4);
         document.getElementById('volume').value = volume;
